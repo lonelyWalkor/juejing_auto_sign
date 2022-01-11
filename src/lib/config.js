0e6842c24e1fe9e0f214f381f0cdb5c5ee5a1a9f
xiaoduan.KD <1961018000@qq.com>
2022-01-11 18:32:56 +0800

    feat:新增加自动获取游戏奖励的功能

--- src/lib/config.js
+++ src/lib/config.js
@@ -1,5 +1,7 @@
 module.exports = {
   COOKIE: process.env.COOKIE || '',
+  // 自动玩游戏需要此参数，在掘金首页打开控制台输入这行代码`window.__NUXT__.state.auth.user.id`就可以得到
+  USERID: process.env.USERID || '',
   DINGTALK_WEBHOOK: process.env.DINGTALK_WEBHOOK || '',
   DINGTALK_SECRET: process.env.DINGTALK_SECRET || '',
   ALL_IN: process.env.ALL_IN || '',
@@ -7,5 +9,5 @@ module.exports = {
   WX_APP_ID: process.env.WX_APP_ID || '', // 应用 ID
   WX_APP_SECRET: process.env.WX_APP_SECRET || '', // 应用 secret
   FEISHU_WEBHOOK: process.env.FEISHU_WEBHOOK || '',
-  FEISHU_SECRET: process.env.FEISHU_SECRET || '',
+  FEISHU_SECRET: process.env.FEISHU_SECRET || ''
 }
